28fcc30f33384a44821c047c21b99bba2eeea0a4
rskwesterman <47602558+rskwesterman@users.noreply.github.com>
2019-04-05 12:32:13 +0200

    Update NewModel.py

--- Final Code Report/NewModel.py
+++ Final Code Report/NewModel.py
@@ -55,7 +55,7 @@ def get_model(lr, kernel_size=(3,3), pool_size=(2,2)):
      model.add(MaxPool2D(pool_size = (3,3)))
        
      model.add(Flatten())
-     model.add(Dense(128, activation = 'relu'))
+     model.add(Dense(64, activation = 'relu'))
      model.add(Dense(64, activation = 'relu'))
      model.add(Dense(1, activation = 'sigmoid'))
 
